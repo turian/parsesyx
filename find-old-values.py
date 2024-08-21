09f15a0f6f9b48ec20c57c9cb6874beb8cfd0803
Joseph Turian <turian@gmail.com>
2024-08-21 17:40:45 +0100

    Added DX7_AllTheWeb_param_freqs.json

--- find-old-values.py
+++ find-old-values.py
@@ -18,6 +18,12 @@ for k in key_values:
         continue
     c = Counter(key_values[k])
     # print({k: [(v, c[v]) for v in sorted(set(key_values[k]))]})
+    # 140 is the inflection point, around 50% percentile
+    # 1932 is 90% percentile
+    # print({k: [(v, c[v]) for v in sorted(set(key_values[k])) if c[v] > 1932]})
+    print({k: [{"value": v, "freq": c[v]} for v in sorted(set(key_values[k]))]})
     # print({k: len(sorted(set(key_values[k])))})
-    for v in sorted(set(key_values[k])):
-        print(c[v])
+
+    # Print ALL freqs, for finding freq cutoff
+#    for v in sorted(set(key_values[k])):
+#        print(c[v])
